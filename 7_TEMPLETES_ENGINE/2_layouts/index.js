e0fc8fde325223513ec19ba373f2f78b11cbc160
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-03-25 22:33:30 -0300

    criado o main.handlebars

---
+++ 7_TEMPLETES_ENGINE/2_layouts/index.js
@@ -0,0 +1,29 @@
+// const express = require('express')
+// const exphbs = require('express-handlebars')
+// const app = express()
+
+// app.engine('handlebars', exphbs.engine())
+// app.set('view engine', 'handlebars')
+
+// app.get('/', (req, res) => {
+//   res.render('home', { layout: false })
+// })
+
+// app.listen(3000, () => {
+//   console.log('Servindo rodando')
+// })
+
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.listen(3000, () => {
+  console.log('Servidor rodando')
+})
