0ced140018224ddc151bb7af57831989d6bc5a11
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-04-04 23:23:28 -0300

    else

--- 7_TEMPLETES_ENGINE/5_elses/index.js
+++ 7_TEMPLETES_ENGINE/5_elses/index.js
@@ -1,3 +1,24 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.get('/dashboard', (req, res) => {
+  res.render('dashboard')
+})
+
+app.get('/', (req, res) => {
+  const auth = true
+  const user = {
+    name: 'vinicius',
+    age: '21'
+  }
+
+  res.render('home', { user, auth })
+})
+
+app.listen(3000, (req, res) => {
+  console.log('Servidor rodando')
+})
