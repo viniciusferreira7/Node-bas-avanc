b38b7b21da97d5919927b6f67291db9d32d97d89
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-05-30 23:20:43 -0300

    conectando o node ao mysql

--- 8_NODE_E_MYSQL/1_instalacao/views/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const exphbs = require('express-handlebars')
-const mysql = require('mysql')
-
-const app = express()
-
-app.engine('handlebars', exphbs())
-app.set('view engine','handlebars')
