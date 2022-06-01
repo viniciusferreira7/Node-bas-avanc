46e3e3682ddebbc1c7f289ed16e001ba2a3ff079
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-05-31 23:32:50 -0300

    criando formulario

--- 8_NODE_E_MYSQL/2_inserindo_dados/index.js
+++ 8_NODE_E_MYSQL/2_inserindo_dados/index.js
@@ -1,3 +1,27 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const mysql = require('mysql')
+
+const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/', (req,res) =>{
+  res.render('home')
+})
+
+const conn = mysql.createConnection({
+  host:'localhost',
+  user:'root',
+  password:'FPeBbNYC*T2*',
+  database:'nodemysql'
+})
+
+conn.connect((err)=> {
+  if(err) console.log(err)
+  console.log('Conectado')
+  app.listen(3000)
+})
