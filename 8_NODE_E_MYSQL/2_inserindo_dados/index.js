86693514bb574964b777b145400d7a742c912ce0
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-06-05 19:26:00 -0300

    dados inseridos no banco

--- 8_NODE_E_MYSQL/2_inserindo_dados/index.js
+++ 8_NODE_E_MYSQL/2_inserindo_dados/index.js
@@ -4,13 +4,35 @@ const mysql = require('mysql')
 
 const app = express()
 
+app.use(express.urlencoded({
+  extended:true,
+}))
+
+app.use(express.json())
+
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 
+
 app.get('/', (req,res) =>{
   res.render('home')
+})
+
+app.post('/books/insertBook', (req,res) => {
+
+  const title = req.body.title
+  const pagesqty = req.body.pagesqty
+
+  const sql = `INSERT INTO books (title, pagesqty) VALUES ('${title}', '${pagesqty}')`
+
+  conn.query(sql, (err)=> {
+    if(err) console.log(err)
+    console.log('Dados inseridos')
+    res.redirect('/')
+   })
+  
 })
 
 const conn = mysql.createConnection({
