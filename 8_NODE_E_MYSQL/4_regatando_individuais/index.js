f2435c98aeaa6bed0e9b90f262b4185e7fc26748
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-06-06 23:28:58 -0300

    pegando os dados individuais parte 1

--- 8_NODE_E_MYSQL/4_regatando_individuais/index.js
+++ 8_NODE_E_MYSQL/4_regatando_individuais/index.js
@@ -4,13 +4,44 @@ const mysql = require('mysql')
 
 const app = express()
 
+app.use(express.urlencoded({
+  extended:true
+}))
+
+app.use(express.json())
+
 app.engine('handlebars', exphbs.engine())
-app.set('view engine',' handlebars')
+app.set('view engine','handlebars')
 
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('home')
+})
+
+app.post('/books/insertBook', (req, res)=>{
+  const title = req.body.title
+  const pagesqty = req.body.pagesqty
+
+  const sql = `INSERT INTO books (title, pagesqty) VALUES ('${title}','${pagesqty}')`
+
+  conn.query(sql, (err)=> {
+    if(err) console.log(err)
+    res.redirect('/')
+  })
+})
+
+app.get('/books', (req, res) => {
+
+  const sql = 'SELECT * FROM books'
+
+  conn.query(sql, (err, data)=> {
+    const books = data
+
+    if(err) console.log(err)
+    console.log(books)
+    res.render('books', {books})
+  })
 })
 
 const conn = mysql.createConnection({
